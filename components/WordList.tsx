598657d40cd3a17556e657a7d81101e14ffa728a
janaSunrise <warriordefenderz@gmail.com>
2022-04-14 15:40:13 +0530

    Refactor empty data.

--- components/WordList.tsx
+++ components/WordList.tsx
@@ -29,27 +29,26 @@ export const WordList = ({ searchText }: WordListProps) => {
     });
   });
 
+  if (filteredData.length === 0) {
+    return <div className="text-center text-gray-500">No words found</div>;
+  }
+
   return (
     <>
-      {filteredData.length === 0 && (
-        <div className="text-center text-gray-500">No words found</div>
-      )}
-
-      {filteredData.length > 0 &&
-        filteredData.map((element: any) => (
-          <h1
-            key={element}
-            onClick={() => navigator.clipboard.writeText(element)}
+      {filteredData.map((element: any) => (
+        <h1
+          key={element}
+          onClick={() => navigator.clipboard.writeText(element)}
+        >
+          <div
+            className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2 "
+            onClick={() => toast.success(`Successfully copied ${element}`)}
           >
-            <div
-              className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2 "
-              onClick={() => toast.success(`Successfully copied ${element}`)}
-            >
-              <div className="flex-grow font-medium px-2">{element}</div>
-            </div>
-            <div className="w-full border-t border-gray-300" />
-          </h1>
-        ))}
+            <div className="flex-grow font-medium px-2">{element}</div>
+          </div>
+          <div className="w-full border-t border-gray-300" />
+        </h1>
+      ))}
     </>
   );
 };
